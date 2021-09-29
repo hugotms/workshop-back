6a006cf74cfedcddbdfcfbe7d352835b8d6adc2e
Rose <rose.heautot@gmail.com>
2021-09-29 14:33:46 +0200

    getBreach.js update

--- getBreach.js
+++ getBreach.js
@@ -4,21 +4,20 @@ const lambda = new AWS.Lambda({region: 'eu-west-3'});
 const fetch = require("node-fetch");
 
 exports.getBreach = async (url) => {
-    const name = getName(url);
-    const breach = await fetch(`https://haveibeenpwned.com/api/v3/breach/${name}`, {
-        method: "POST",
-    })
-        .then(r => r.json())
-        .then(r => {
-            console.log(r);
-            return r
-        }).catch(e => console.log(e));
     try {
+        const name = getName(url);
+        const breach = await fetch(`https://haveibeenpwned.com/api/v3/breach/${name}`, {
+            method: "GET",
+        })
+            .then(r => r.json())
+            .then(r => r).catch(e => {
+                throw e;
+            });
         return JSON.stringify({
             statusCode: 200,
             data: {
                 name: breach.Name,
-                breaches : breach.PwnCount,
+                count: breach.PwnCount,
                 lastDate: breach.BreachDate,
                 elements: breach.DataClasses
             }
@@ -26,7 +25,7 @@ exports.getBreach = async (url) => {
     } catch (err) {
         return JSON.stringify({
             statusCode: 404,
-            data: 'Unable to proceed information'
+            data: err
         });
     }
 }
@@ -38,4 +37,3 @@ const getName = (url) => {
     tempurl = tempurl[1];
     return tempurl;
 }
-
